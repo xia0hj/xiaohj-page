12e6de11d169acf8daf789fec7e5f60b5960672a
xiaohj <719887163@qq.com>
2022-07-08 18:51:55 +0800

    aaa

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # 应为我本地有两个git，我学习的git命名是git@study.github.com
-git push -f git@github.com:xia0hj/xiaohj-page.git main
+git push -f git@github.com:xia0hj/xiaohj-page.git main:gh-page
 #git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
 
 cd -
